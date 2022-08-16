77dba79b3615b92e2056b9c0534bc1d84b0a9200
MARCOS <marcoslopezsanchez@hotmail.com>
2022-08-16 09:25:05 -0500

    Add componets

---
+++ src/components/Layout/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Layout = () => {
+  return (
+    <div>
+      <h1>Layaut</h1>
+    </div>
+  );
+}
---
+++ src/components/MenList/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function MenList() {
+  return (
+    <div>
+      <h1>MenList</h1>
+    </div>
+  );
+}
