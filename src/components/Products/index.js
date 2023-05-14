272feaa7420e2dc616478dc310588becbf83b3c4
MARCOS <marcoslopezsanchez@hotmail.com>
2023-05-14 10:43:18 -0600

    Add Section
    Add import Description

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -1,82 +1,106 @@
 import React from "react";
-import { 
+import {
   Container,
   Panel,
   Column,
   Galeria,
-  Description
-  
- } from "./styles";
+  Description,
+  Section
+} from "./styles";
 
 export const Products = () => {
   return (
     <Container>
       <Panel>
         <Column>
-        <Galeria>
-          <img src="https://http2.mlstatic.com/D_NQ_NP_826904-MLM54973846403_042023-O.webp" alt="producto"/>
-        </Galeria>
-        <Info/>
+          <Galeria>
+            <img src="https://http2.mlstatic.com/D_NQ_NP_826904-MLM54973846403_042023-O.webp" alt="producto" />
+          </Galeria>
+          <Info />
         </Column>
         <Column>
-        {/* <ProductsVent/> */}
-        {/* <InfoVendedor/> */}
-        {/* <Galeria/> */}
+          {/* <ProductsVent/> */}
+          {/* <InfoVendedor/> */}
+          <Garantia />
         </Column>
       </Panel>
     </Container>
   )
 }
 
-const Info =()=>{
-  return(
+const Info = () => {
+  return (
     <Description>
-      <h4>Descripción</h4>
-      <p>OutletComputer de Mexico le ofrece equipos Apple reacondicionados a nuevo, 100% funcionales para Profesionales del Audio, Video, animacion, videos, clases en linea y mucho mas.... Son equipos 2019 que regresaron por el sistema de intercambio y se trabajan para ser funcionales y actuales al 2023.
+      <h4>Descripción</h4> <br />
+      <p>OutletComputer de Mexico le ofrece equipos Apple reacondicionados a nuevo, 100% funcionales para Profesionales del Audio,
+        Video, animacion, videos, clases en linea y mucho mas.... Son equipos 2019 que regresaron por el sistema de intercambio y se
+        trabajan para ser funcionales y actuales al 2023.<p /> <br />
 
-Son equipos ideales para profesionales , donde todo esta equilibrado para que no se corte la transmision por fallas en el hardware, todo integrado en su maquina.
-Son equipos Imac 2019 , Core i9 con 64gb RAM y disco SSD 2tb , ademas de una Tarjeta Grafica de 4gb.
-Vienen con teclado Apple recargable y Mouse Inalambrico de pilas , ademas de su cable de poder y una caja para su proteccion en el envio por MercadoEnvios.
-Le ofrece OutletComputer de Mexico 3 meses de garantia ya que el equipo pasa los testeos de funcionamiento que nuestros Tecnicos graduados le hacen a los equipos para que esten 100% funcionales.
-</p>
-<p>Processor Speed 3.6 GHz
-Built-in Display 27
-Model Identifier iMac19,1
-Apple Order Number BTO (Build to Order)
-Apple Model Number A2115 (EMC 3194)
-Form Factor 27 iMac Late 2012
-Apple Subfamily Retina 5K, Early 2019
-EXPAND / UPGRADE
-Standard RAM
-64 GB
-Maximum RAM
-128 GB
-RAM Slots 4
-RAM Specs PC4-21300 DDR4-2666
-RAM Speed Speed 2666 MHz
-Standard Storage Drive
-2tb SSD
-Storage Drive Interface Serial ATA (SATA) (6 Gb/s)/PCIe
-SYSTEM
-Processor Type Core i9 (9900K) Coffee Lake
-Processors / Cores 1 (8 Cores)
-Geekbench Rating N/A
-Turbo Boost Speed 5.0 GHz
-Custom Speeds N/A
-Architecture 64-Bit
-Processor Upgradeable LGA 1151 socket
-FPU Integrated
-Integrated System RAM None
-Standard RAM 64 GB
-Maximum RAM 128 GB
-RAM Slots 4
-ROM / Firmware Type EFI
-EFI Architecture 64-Bit
-L1 Cache 32k/32k
-L2 / L3 Cache 256k (x4) 6 MB (on chip)</p>
-<p>
-  Con OutletComputer de Mexico recibira lo mejor de nuestro equipo para obtener la mejor experiencia en su compra.
-</p>
+        Son equipos ideales para profesionales , donde todo esta equilibrado para que no se corte la transmision por fallas en el hardware,
+        todo integrado en su maquina.<br />
+        Son equipos Imac 2019 , Core i9 con 64gb RAM y disco SSD 2tb , ademas de una Tarjeta Grafica de 4gb.<br />
+        Vienen con teclado Apple recargable y Mouse Inalambrico de pilas , ademas de su cable de poder y una caja para su proteccion en el
+        envio por MercadoEnvios.
+        Le ofrece OutletComputer de Mexico 3 meses de garantia ya que el equipo pasa los testeos de funcionamiento que nuestros Tecnicos
+        graduados le hacen a los equipos para que esten 100% funcionales.
+
+        <br /><br />
+        Processor Speed 3.6 GHz<br />
+        Built-in Display 27<br />
+        Model Identifier iMac19,1
+        Apple Order Number BTO (Build to Order)<br />
+        Apple Model Number A2115 (EMC 3194)<br />
+        Form Factor 27 iMac Late 2012<br />
+        Apple Subfamily Retina 5K, Early 2019<br />
+        EXPAND / UPGRADE<br />
+        Standard RAM<br />
+        64 GB<br />
+        Maximum RAM<br />
+        128 GB<br />
+        RAM Slots 4<br />
+        RAM Specs PC4-21300 DDR4-2666<br />
+        RAM Speed Speed 2666 MHz<br />
+        Standard Storage Drive<br />
+        2tb SSD<br />
+        Storage Drive Interface Serial ATA (SATA) (6 Gb/s)/PCIe<br />
+        SYSTEM<br />
+        Processor Type Core i9 (9900K) Coffee Lake<br />
+        Processors / Cores 1 (8 Cores)<br />
+        Geekbench Rating N/A<br />
+        Turbo Boost Speed 5.0 GHz<br />
+        Custom Speeds N/A<br />
+        Architecture 64-Bit<br />
+        Processor Upgradeable LGA 1151 socket<br />
+        FPU Integrated<br />
+        Integrated System RAM None<br />
+        Standard RAM 64 GB<br />
+        Maximum RAM 128 GB<br />
+        RAM Slots 4<br />
+        ROM / Firmware Type EFI<br />
+        EFI Architecture 64-Bit<br />
+        L1 Cache 32k/32k<br />
+        L2 / L3 Cache 256k (x4) 6 MB (on chip)<br /><br />
+
+        Con OutletComputer de Mexico recibira lo mejor de nuestro equipo para obtener la mejor experiencia en su compra.
+      </p>
     </Description>
+  )
+}
+const Garantia = () => {
+  return (
+    <Section>
+      <h5>Garantía</h5>
+      <div>
+        <span>
+          <p className="title">Compra Protegida con Mercado Pago</p>
+          <p className="description">Recibe el producto que esperabas o te devolvemos tu dinero</p>
+        </span>
+        <span>
+          <p className="title">Garantía del vendedor</p>
+          <p className="description">Garantía del vendedor: 3 meses</p>
+        </span>
+        <a href="#">Conocer más sobre garantía</a>
+      </div>
+    </Section>
   )
 }
--- src/components/Products/styles.js
+++ src/components/Products/styles.js
@@ -1,21 +1,21 @@
 import styled from "styled-components";
 
-export const  Container = styled.div`
+export const Container = styled.div`
 display: flex:;
 flex-direction: column;
 margin-top: 12px;
 border: 1px solid red;
 `
-export const  Panel = styled.div` 
+export const Panel = styled.div` 
 background-color: var(--white);
 box-shadow: var(--panel);
 display: grid;
 grid-template-column: 67fr 33fr;
 `
-export const  Column = styled.div` 
+export const Column = styled.div` 
 
 `
-export const  Galeria = styled.div`
+export const Galeria = styled.div`
 display: flex;
 align-items: center;
 justify-content: center;
@@ -27,4 +27,44 @@ height: 530px;
 `
 export const Description = styled.div`
 border-top : 1px solid var(--border);
+width: 88%;
+margin:0 auto;
+padding: 40px 0;
+>h4{
+    color: var(--text1);
+    font-size: 26px;
+    margin-bottom: 32px;
+    line-height: 30px:
+}
+>p, span{
+    line-height: 27px;
+    font-size: 20px;
+    color: var(--text3);
+}
+>hr{
+    border-top{
+
+    }
+}
+`
+export const Section = styled.div`
+padding: 32px 16px;
+margin: 0 16px;
+border: 1px solid red;
+border-bottom: none;
+>h5{
+    font-size: 18px;
+    margin-bottom: 28px;
+    color: var(--text1);
+    line-height: 22.5px;
+}
+>div{
+    display: flex;
+    flex-direction: column;
+    line-height: 18.9px;
+
+    >span + span{
+        margin-top: 24px;
+    }
+}
 `
