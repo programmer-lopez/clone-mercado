e040c304a8241706617e34f582c89c61cc60c5e3
MARCOS <marcoslopezsanchez@hotmail.com>
2023-05-14 18:56:51 -0600

    Update  infoVendedor
    Add styled.js icons and styled cool.

--- src/components/InfoVendedor/styled.js
+++ src/components/InfoVendedor/styled.js
@@ -1,10 +1,15 @@
-import styled from "styled-components";
-import {
-    IoMdRibbon,
+import styled, {css} from "styled-components";
+import { IoMdRibbon } from "react-icons/io";
+import { 
+    HiOutlineLocationMarker,
     HiOutlineChatAlt2,
     HiOutlineClock
-} from "react-icons/io";
-import { HiOutlineLocationMarker } from "react-icons/hi";
+} from "react-icons/hi";
+
+const iconsCss = css`
+width: 25px;
+height: 25px;
+`
 
 export const Container = styled.div`
 padding: 32px 16px;
@@ -44,20 +49,68 @@ export const Card = styled.div`
 
     }
     `
-export const LocationIcon = styled.div`
-    
+export const LocationIcon = styled(HiOutlineLocationMarker)`
+    opacity: .5;
+    margin-top: -1.3rem;
+    ${iconsCss}
     `
-export const PlatinumIcon = styled.div`
-    
+export const PlatinumIcon = styled(IoMdRibbon)`
+    color: var(--green);
+    margin-top: -1rem;
+    opacity: .8;
+    ${iconsCss}
     `
 export const Reputacion = styled.div`
-    
+    flex-direction: column;
+    align-items: center;
     `
-export const Cajas = styled.div`
-    
+export const Cajas = styled.ol`
+    list-style: none;
+    width: 100%;
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    grid-gap: 0px;
+    padding: 0 4px;
+    >li{
+        width: 100%;
+        height: 8px;
+         
+
+        &:nth-child(1){
+            background-color: var(--reputacion1);
+        }
+        &:nth-child(2){
+            background-color: var(--reputacion2);
+        }
+        &:nth-child(3){
+            background-color: var(--reputacion3);
+        }
+        &:nth-child(4){
+            background-color: var(--reputacion4);
+        }
+        &:nth-child(5){
+            background-color: var(--reputacion5);
+        }
+        &.active{
+            height: 12px;
+        }
+    }    
     `
 export const Row = styled.div`
-    
+    margin-top: 10px;
+    display: flex;
+    alight-items: flex-start;
+    >div{
+        width: 33%;
+        padding: 0 10px;
+        display: flex;
+        flex-direction: column;
+        text-align: center;
+        >strong{
+            font-size: 24%;
+            font-weight: normal;
+        }
+    }
     `
 export const SupporIcon = styled.div`
     
