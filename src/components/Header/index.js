f85c9d21f28cc1a72e7ededb733deda304ab3836
Marcos Imac <marcoslopezsanchez@hotmail.com>
2023-07-19 11:53:16 -0600

    sumary

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,8 +6,12 @@ export const Header = () => {
   return (
     <Container>
       <Wrapper>
-        <img
+        <img className="nav-logo"
           src="https://http2.mlstatic.com/frontend-assets/ml-web-navigation/ui-navigation/5.19.5/mercadolibre/logo__large_plus.png"
+          alt="logo"
+        />
+        <img className="img-2"
+          src="https://http2.mlstatic.com/D_NQ_948848-MLA70392170748_072023-OO.webp"
           alt="logo"
         />
       </Wrapper>
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,17 +1,24 @@
 import styled from "styled-components";
 
 export const Container = styled.header`
+height:100px;
 width: 100vw;
 background-color: var(--yellow);
-height:100px;
+
 `
 export const Wrapper = styled.nav`
 max-width: 1200px;
 padding: 8px 8px;
 margin: 0 auto;
 height: 91px;
-img{
+>.nav-logo{
     width: 134px;
     height: 33px;
+}
+.img-2{
+    width: 390px;
+    height: 40px;
+    display: grid;
+    flex-direction: R
 }
 `
